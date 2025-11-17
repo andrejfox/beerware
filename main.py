18ae1e0bfd854638e7a1fbc9f2ed9c867479b5ad
andrejfox <andrej.vencelj.007@gmail.com>
2025-11-17 17:10:51 +0100

    updated TempReader class

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 import sys
 
-from PySide6.QtCore import QSize, QThread, Signal, QEvent
-from PySide6.QtGui import QIcon, QFont, QPixmap, QTouchEvent
+from PySide6.QtCore import QSize, QThread, Signal
+from PySide6.QtGui import QIcon, QFont, QPixmap
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from w1thermsensor import W1ThermSensor
-from PySide6.QtCore import Qt
 
 
-# set the size of the window
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 1024
 
-# set the size of the buttons
-BUTTON_HEIGHT = 100
-BUTTON_WIDTH = 300
-
-# default target temp
-TARGET_TEMP = 40
-CURR_TEMP = 32
+DEFAULT_TARGET_TEMP = 40
 
 class TempReader(QThread):
-    new_temp = Signal(float)
+    cur_temp = Signal(float)  # signal emits new temperature
 
     def __init__(self, sensor):
         super().__init__()
@@ -30,11 +22,15 @@ class TempReader(QThread):
     def run(self):
         while self.running:
             try:
-                temp = self.sensor.get_temperature()
-                self.new_temp.emit(temp)
-            except:
-                pass
-            self.msleep(50)  # update 5 times per second
+                temperature = self.sensor.get_temperature()
+                self.cur_temp.emit(temperature)
+            except Exception as e:
+                print(f"Sensor error: {e}")
+            self.msleep(100)
+
+    def stop(self):
+        self.running = False
+        self.wait()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -44,15 +40,15 @@ class MainWindow(QMainWindow):
         self.setFixedSize(QSize(WINDOW_WIDTH, WINDOW_HEIGHT))
         self.setWindowIcon(QIcon("pics/beer_pic.png"))
         self.setStyleSheet("""
-                background-color: #2D2C2E;
-                color: #FBBD0D;
-                """)
+            background-color: #2D2C2E;
+            color: #FBBD0D;
+        """)
 
         sensors = W1ThermSensor.get_available_sensors()
 
         print(f"Found {len(sensors)} sensors")
         for sensor in sensors:
-            print(sensor.id)  # unique sensor ID
+            print(sensor.id)
 
         self.sensor = W1ThermSensor()
 
@@ -67,27 +63,27 @@ class MainWindow(QMainWindow):
         self.temp_label.move(0, 50)  # position it somewhere
 
         self.sensor_thread = TempReader(self.sensor)
-        self.sensor_thread.new_temp.connect(self.update_temp)
+        self.sensor_thread.temp.connect(self.update_temp)
         self.sensor_thread.start()
 
-        self.temp_target = QLabel(f'Target temp: {TARGET_TEMP:.1f} °C', self)
+        self.temp_target = QLabel(f'Target temp: {DEFAULT_TARGET_TEMP:.1f} °C', self)
         self.temp_target.setFont(QFont("Roboto", 32))
         self.temp_target.adjustSize()
 
         b_plus = QPushButton("+", self)
-        b_plus.setGeometry(0, WINDOW_HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
+        b_plus.setGeometry(0, self.height() - 100, 200, 100)
         b_plus.clicked.connect(self.b_plus_clicked)
 
         b_minus = QPushButton("-", self)
-        b_minus.setGeometry(WINDOW_WIDTH - BUTTON_WIDTH, WINDOW_HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
+        b_minus.setGeometry(self.width() - 200, self.height() - 100, 200, 100)
         b_minus.clicked.connect(self.b_minus_clicked)
 
         # --- EXIT BUTTON ---
         exit_button = QPushButton("EXIT", self)
         exit_button.setStyleSheet("background-color: red; color: white; font-size: 32px;")
         exit_button.setGeometry(
-            (WINDOW_WIDTH // 2) - 150,  # center X
-            WINDOW_HEIGHT - BUTTON_HEIGHT - 120,  # slightly above bottom
+            (self.width() // 2) - 150,  # center X
+            self.height() - 100 - 120,  # slightly above bottom
             300, 100  # size
         )
         exit_button.clicked.connect(self.exit_app)
@@ -108,20 +104,20 @@ class MainWindow(QMainWindow):
         self.temp_label.setText(f"Current temp: {current_temp:.2f} °C")
         self.temp_label.adjustSize()
 
-        if current_temp < TARGET_TEMP:
+        if current_temp < DEFAULT_TARGET_TEMP:
             self.heating_on()
         else:
             self.heating_off()
 
     def b_plus_clicked(self):
-        global TARGET_TEMP
-        TARGET_TEMP += 1
-        self.temp_target.setText(f'Target temp: {TARGET_TEMP} °C')
+        global DEFAULT_TARGET_TEMP
+        DEFAULT_TARGET_TEMP += 1
+        self.temp_target.setText(f'Target temp: {DEFAULT_TARGET_TEMP} °C')
 
     def b_minus_clicked(self):
-        global TARGET_TEMP
-        TARGET_TEMP -= 1
-        self.temp_target.setText(f'Target temp: {TARGET_TEMP} °C')
+        global DEFAULT_TARGET_TEMP
+        DEFAULT_TARGET_TEMP -= 1
+        self.temp_target.setText(f'Target temp: {DEFAULT_TARGET_TEMP} °C')
 
     def heating_on(self):
         self.heating.setPixmap(QPixmap("pics/heating_on.png"))
