1c8f1039a97e50eaee1327e646feb7fc8fbe6360
andrejfox <andrej.vencelj.007@gmail.com>
2025-11-17 15:20:42 +0100

    updates start.sh

--- startup/start_beerware.sh
+++ startup/start_beerware.sh
@@ -2,10 +2,16 @@
 
 cd /home/admin/beerware || exit 1
 
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+fi
+
 source .venv/bin/activate
 
-# Install deps ONLY if not already installed
 if [ ! -f ".venv/packages_installed.flag" ]; then
+    echo "Installing requirements..."
+    pip install --upgrade pip
     pip install -r requirements.txt
     touch .venv/packages_installed.flag
 fi
