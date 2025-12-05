1d14fe7b3abf6224f81750431d57ae5e12b9ac42
pc-rampa <andrej.vencelj.007@gmail.com>
2025-12-05 18:09:33 +0100

    added some stuff

--- src/thermometers.py
+++ src/thermometers.py
@@ -19,8 +19,6 @@ class Thermometers:
         self._thread = None
 
     def _update_loop(self):
-        last_rescan = time.time()
-
         while self._running:
             for sensor in list(self.sensors):
                 try:
@@ -29,30 +27,8 @@ class Thermometers:
                     print(f"Error reading sensor {sensor.id}: {e}")
                     self.temperatures[sensor.id] = None
 
-            if time.time() - last_rescan >= 5:
-                self._rescan_sensors()
-                last_rescan = time.time()
-
             print(self.temperatures)
             time.sleep(self.update_interval)
-
-    def _rescan_sensors(self):
-        found = W1ThermSensor.get_available_sensors()
-        found_ids = {s.id for s in found}
-        current_ids = {s.id for s in self.sensors}
-
-        for sensor in found:
-            if sensor.id not in current_ids:
-                print(f"[+] New sensor detected: {sensor.id}")
-                self.sensors.append(sensor)
-                self.temperatures[sensor.id] = None
-
-        removed_ids = current_ids - found_ids
-        if removed_ids:
-            for sid in removed_ids:
-                print(f"[-] Sensor removed: {sid}")
-                self.temperatures.pop(sid, None)
-            self.sensors = [s for s in self.sensors if s.id in found_ids]
 
     def start(self):
         if not self._running:
